fefb37c91c83dce6bd8bc16ed1085c697879e498
= <=>
2024-03-25 20:44:59 -0400

    added drinks

--- 03_25_24/main.cpp
+++ 03_25_24/main.cpp
@@ -7,5 +7,11 @@ int main()
     std::string descript = "My product";
     product p(3.99, descript, prodNum);
     std::cout << p.tostring() << std::endl;
+    drink d(COFFEE, HOT, LARGE, "Skim Milk", "Brown Sugar");
+    std::cout << d.tostring() << std::endl;
+    drink myDrink(TEA, ICE, SMALL, "none", "none", "Small Iced Tea", "tea1234");
+    std::cout << myDrink.tostring() << std::endl;
+    std::cout << myDrink.getDescription() << std::endl;
+
     return 0;
 }
--- 03_25_24/product.h
+++ 03_25_24/product.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <algorithm>
 
 class product
 {
@@ -18,11 +19,72 @@ public:
     void setDescription(std::string);
     std::string tostring() const;
 
-private:
+protected:
     double price;
     std::string description;
     std::string prodNum;
     bool purchased;
+};
+
+enum baseType
+{
+    COFFEE,
+    TEA,
+    CREAM
+};
+enum sizeType
+{
+    SMALL,
+    MED,
+    LARGE
+};
+enum tempType
+{
+    HOT,
+    ICE,
+    BLEND
+};
+const int ENUM_NUM = 3;
+const baseType bases[] = {COFFEE,
+                          TEA,
+                          CREAM};
+const std::string baseStr[] = {"Coffee", "Tea", "Cream"};
+const std::string strBase[] = {"COFFEE", "TEA", "CREAM"};
+const sizeType sizes[] = {SMALL,
+                          MED,
+                          LARGE};
+const std::string sizeStr[] = {"Small", "Medium", "Large"};
+const tempType temps[] = {HOT,
+                          ICE,
+                          BLEND};
+const std::string tempStr[] = {"Hot", "Iced", "Blended"};
+
+class drink : public product
+{
+public:
+    drink(baseType, tempType, sizeType, std::string dairy, std::string flavor);
+    drink(baseType, tempType, sizeType, std::string dairy, std::string flavor, std::string description, std::string prodNum);
+    // drink();
+    std::string getBaseStr() const;
+    std::string getTemperature() const;
+    std::string getSize() const;
+    std::string getDairy() const;
+    std::string getFlavor() const;
+    double getPrice() const;
+    void setBase(baseType);
+    void setTemperature(tempType);
+    void setSize(sizeType);
+    void setDairy(std::string);
+    void setFlavor(std::string);
+    std::string tostring() const;
+
+private:
+    baseType base;
+    tempType temperature;
+    sizeType size;
+    std::string dairy;
+    std::string flavor;
+    static int numDrinks;
 };
 
 #endif
